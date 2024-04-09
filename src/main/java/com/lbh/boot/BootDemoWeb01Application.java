212ed9e43d325d785ad5bf282c686462b9c7ff7d
lbh <997939361@qq.com>
2024-04-09 21:18:22 +0800

    添加run方法的返回值

--- src/main/java/com/lbh/boot/BootDemoWeb01Application.java
+++ src/main/java/com/lbh/boot/BootDemoWeb01Application.java
@@ -2,12 +2,14 @@ package com.lbh.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BootDemoWeb01Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(BootDemoWeb01Application.class, args);
+
+        ConfigurableApplicationContext c= SpringApplication.run(BootDemoWeb01Application.class, args);
     }
 
 }
