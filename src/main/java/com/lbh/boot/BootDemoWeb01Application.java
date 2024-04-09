ec50e2b2bacc1804904bca08064ba00b05c69376
smw <1733812267@qq.com>
2024-04-09 20:57:18 +0800

    Initial commit

---
+++ src/main/java/com/lbh/boot/BootDemoWeb01Application.java
@@ -0,0 +1,13 @@
+package com.lbh.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDemoWeb01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootDemoWeb01Application.class, args);
+    }
+
+}
